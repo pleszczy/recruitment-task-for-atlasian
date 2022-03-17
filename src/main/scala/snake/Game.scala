94880f01ce8b1f7972b54fcbbf2ce530a673b578
Piotr <piotrkun@gmail.com>
2022-03-17 22:56:11 +0100

    Extracting game state calculation to constructor

--- src/main/scala/snake/Game.scala
+++ src/main/scala/snake/Game.scala
@@ -2,58 +2,43 @@ package org.atlassian
 package snake
 
 import snake.Game.SnakeDirection
-import snake.Game.SnakeDirection.{DOWN, LEFT, RIGHT, UP}
+import snake.Game.SnakeDirection.{DOWN, LEFT, RIGHT, UP, NONE}
 
 object Game {
 
   enum SnakeDirection:
-    case UP, DOWN, LEFT, RIGHT
+    case UP, DOWN, LEFT, RIGHT, NONE
 
-  class Snake(val x: Int = 1, val y: Int = 1, val noOfMoves: Int = 0, val gameOver: Boolean = false, val previousMove: SnakeDirection = UP) {
-    def right() = {
-      if (size() > 1 && previousMove == LEFT) {
-        Snake(x + 1, y, noOfMoves + 1, gameOver = true, previousMove = RIGHT)
+  class Snake(val x: Int = 1, val y: Int = 1, val noOfMoves: Int = 0, val gameOver: Boolean = false, val previousMove: SnakeDirection = NONE, val currentMove: SnakeDirection = NONE) {
+    def right() = Snake(x + 1, y, noOfMoves + 1, gameOver, previousMove = currentMove, currentMove = RIGHT)
 
-      } else {
-        Snake(x + 1, y, noOfMoves + 1, gameOver = isGameOver(x + 1, y), previousMove = RIGHT)
-      }
-    }
+    def left() = Snake(x - 1, y, noOfMoves + 1, gameOver, previousMove = currentMove, currentMove = LEFT)
 
-    def left() = {
-      if (size() > 1 && previousMove == RIGHT
-      )
-      {
-        Snake(x - 1, y, noOfMoves + 1, gameOver = true, previousMove = LEFT)
-      }
-      else
-      {
-        Snake(x - 1, y, noOfMoves + 1, gameOver = isGameOver(x - 1, y), previousMove = LEFT)
-      }
-    }
+    def up(): Snake = Snake(x, y - 1, noOfMoves + 1, gameOver, previousMove = currentMove, currentMove = UP)
 
-    def up(): Snake = {
-      if (size() > 1 && previousMove == DOWN) {
-        Snake(x, y - 1, noOfMoves + 1, gameOver = true, previousMove = UP)
-      } else {
-        Snake(x, y - 1, noOfMoves + 1, gameOver = isGameOver(x, y - 1), previousMove = UP)
-      }
-    }
+    def down(): Snake = Snake(x, y + 1, noOfMoves + 1, gameOver, previousMove = currentMove, currentMove = DOWN)
+  }
 
-    def down(): Snake = {
-      if (size() > 1 && previousMove == UP) {
-        Snake(x, y + 1, noOfMoves + 1, gameOver = true, previousMove = DOWN)
-      } else {
-        Snake(x, y + 1, noOfMoves + 1, gameOver = isGameOver(x, y + 1), previousMove = DOWN)
-      }
-    }
-
-    private def isGameOver(nextX: Int, nextY: Int) = {
-      gameOver || !(isWithinBoundaries(nextX) && isWithinBoundaries(nextY))
-    }
-
-    private def size(): Int = (noOfMoves / 5) + 1
+  object Snake {
+    private def size(noOfMoves: Int): Int = (noOfMoves / 5) + 1
 
     private def isWithinBoundaries(num: Int) = if (num >= 1 && num <= 4) then true else false
 
+    private def isGameOver(nextX: Int, nextY: Int) = !(Snake.isWithinBoundaries(nextX) && Snake.isWithinBoundaries(nextY))
+
+    def apply(x: Int = 1, y: Int = 1, noOfMoves: Int = 0, gameOver: Boolean = false, previousMove: SnakeDirection = NONE, currentMove: SnakeDirection = NONE): Snake = {
+      val size = Snake.size(noOfMoves)
+      val gameState = calculateGameState(x, y, gameOver, previousMove, currentMove, size)
+      new Snake(x, y, noOfMoves, gameState, previousMove, currentMove)
+    }
+
+    private def calculateGameState(x: Int, y: Int, gameOver: Boolean, previousMove: SnakeDirection, currentMove: SnakeDirection, size: Int) =
+      (previousMove, currentMove) match {
+        case (UP, DOWN) if size > 1 => true
+        case (DOWN, UP) if size > 1 => true
+        case (RIGHT, LEFT) if size > 1 => true
+        case (LEFT, RIGHT) if size > 1 => true
+        case (_, _) => isGameOver(x, y) || gameOver
+      }
   }
 }
